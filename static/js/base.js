d7f27669c0f48581f55c62097f47534b06a983c7
linxixuan <zmx.morgan@gmail.com>
2013-11-05 22:04:53 +0800

    fix

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,32 @@
 $(function () {
+    // node
+    var ndEditer = $('.editer'),
+        ndViewer = $('.viewer'),
+        ndTargetContainer = ndViewer.one('.container');
+
+    // params
+
     console.log('loaded!');
-})
+    
+    bindEvent();
+
+    function bindEvent() {
+        // viewer event
+        ndViewer.delegate('.container', 'click', handleFocusContainer);
+
+        // editer event
+        ndEditer.delegate('.add-record', 'click', handAddRecord);
+    }
+    
+    // focus the container
+    function handleFocusContainer() {
+        ndViewer.find('.container').removeClass('selected');
+        ndTargetContainer = $(this);
+        $(this).addClass('selected');
+    }
+
+    // add record
+    function handAddRecord() {
+        console.log(1);
+    }
+});
