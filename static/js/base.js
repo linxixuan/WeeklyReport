681efdda076dad184814ffefe2430f347187c7f9
linxixuan <zmx.morgan@gmail.com>
2013-11-19 20:39:52 +0800

    add report object

--- static/js/base.js
+++ static/js/base.js
@@ -2,7 +2,9 @@ $(function () {
     // node
     var ndEditer = $('.editer'),
         ndViewer = $('.viewer'),
-        ndTargetContainer = $(ndViewer.find('.container')[0]);
+        ndTargetContainer = $(ndViewer.find('.container')[0]),
+        report = {},
+        itemIndex = 0;
 
     // params
 
@@ -27,16 +29,24 @@ $(function () {
 
     // add record
     function handAddRecord() {
-        console.log(ndTargetContainer.attr('class'));
         var template = $('#list-item-markup').html(),
+            modified = itemIndex % 2 === 1 ? ' item--even' : '',
             title = $(ndEditer.find('.input')[0]).val(), 
             progress = $(ndEditer.find('.input')[1]).val(), 
             type = $(ndEditer.find('.input')[2]).val(), 
             description = $(ndEditer.find('.input')[3]).val(),
             content;
-        content = mergeArry(template.split('*'), [title, progress, type, description]).join('');
+        content = mergeArry(template.split('*'), [modified, title, progress, type, description]).join('');
 
+        updateReport(content);
         ndTargetContainer.append(content);
+        itemIndex++;
+    }
+
+    // update Report Object
+    function updateReport(content) {
+        var key = ndTargetContainer.attr("class").split(" ")[0];
+        report[key] += content;
     }
 
     function mergeArry(a, b) {
@@ -52,4 +62,5 @@ $(function () {
 
         return c;
     }
+
 });
